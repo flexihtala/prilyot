c3c4a9f2d3867892370f8dc23dec8baae1a706e6
Моховиков Максим Дмитриевич <mokhovikov@tochka.com>
2026-06-06 16:52:28 +0500

    feat: добавил урон

--- game/entities/monster.py
+++ game/entities/monster.py
@@ -1,5 +1,5 @@
 import random
-
+from datetime import datetime, UTC, timedelta
 from pygame import Surface, Rect, transform, image
 from pygame.math import Vector2
 
@@ -11,15 +11,18 @@ class Monster(BaseEntity):
         self.position = position
         self.width = 96
         self.height = 192
-        self.sprite_image = Rect(
-            self.position[0], self.position[1], self.width, self.height
-        )
+        self.hitbox = Rect(*self.position, self.width, self.height)
         image_name = self._get_image()
         self.image = transform.scale(image.load(image_name).convert_alpha(), (96, 192))
         self.speed_coef = 2
 
+        self.damage = 10
+        self.last_damage_at: datetime | None = None
+
     def update(self, game_state):
-        player_pos = game_state.player.position
+        player = game_state.player
+
+        player_pos = player.position
         movement_vector = Vector2(
             player_pos[0] - self.position[0], player_pos[1] - self.position[1]
         )
@@ -29,12 +32,17 @@ class Monster(BaseEntity):
             self.position[1] + norm_mov_vector[1] * self.speed_coef,
         ]
         self.position = new_position
-        self.sprite_image = Rect(
-            self.position[0], self.position[1], self.width, self.height
-        )
+        self.hitbox = Rect(self.position[0], self.position[1], self.width, self.height)
+
+        if player.hitbox.colliderect(self.hitbox):
+            if not self.last_damage_at or (
+                (datetime.now(UTC) - self.last_damage_at) > timedelta(seconds=1)
+            ):
+                player.set_damage(self.damage)
+                self.last_damage_at = datetime.now(UTC)
 
     def render(self, screen: Surface):
-        screen.blit(self.image, self.sprite_image)
+        screen.blit(self.image, self.hitbox)
 
     def _get_image(self):
         number = random.randint(1, 5)
